f9f82a2bd3795a284b336edd005cc77187017716
MUHAMMAD MALFIN <malvin.arkade7@gmail.com>
2025-05-20 23:39:01 +0700

    add Search Contact API

--- src/controller/contact-controller.ts
+++ src/controller/contact-controller.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Response } from "express";
 import { UserRequest } from "../type/user-request";
-import { CreateContactRequest, UpdateContactRequest } from "../model/contact-model";
+import { CreateContactRequest, SearchContactRequest, UpdateContactRequest } from "../model/contact-model";
 import { ContactService } from "../service/contact-service";
 
 export class ContactController {
@@ -58,6 +58,28 @@ export class ContactController {
         success: true,
         message: "Contact Removed successfully",
         data: "OK"
+      })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  static async search(req: UserRequest, res: Response, next: NextFunction) {
+    try {
+      const request: SearchContactRequest = {
+        name: req.query.name as string,
+        email: req.query.email as string,
+        phone: req.query.phone as string,
+        page: req.query.page ? Number(req.query.page) : 1,
+        size: req.query.size ? Number(req.query.size) : 10,
+      }
+
+      const response = await ContactService.search(req.user!, request);
+      res.status(200).json({
+        success: true,
+        message: response.data.length > 0 ? "Contacts found" : "No matching contacts found",
+        data: response.data,
+        paging: response.paging
       })
     } catch (error) {
       next(error)
