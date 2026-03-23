0c06fca26d58bd0ec38d27227a835ef464c1a3d8
MUHAMMAD MALFIN <malvin.arkade7@gmail.com>
2026-03-23 08:16:32 +0700

    feat: setup cors and docker

--- src/application/web.ts
+++ src/application/web.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import { publicRouter } from '../route/public-api';
 import { errorMiddleware } from '../middleware/error-middlerware';
 import { apiRouter } from '../route/api';
+import cors from "cors";
 
 export const web = express();
 web.use(express.json());
+web.use(cors());
 web.use(publicRouter);
 web.use(apiRouter);
 web.use(errorMiddleware);
