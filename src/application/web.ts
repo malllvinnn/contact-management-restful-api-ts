8987e69298b98e799b0a54a4bed3891b7c8bdbaf
MUHAMMAD MALFIN <malvin.arkade7@gmail.com>
2026-03-24 18:40:15 +0700

    update docker compose and application

--- src/application/web.ts
+++ src/application/web.ts
@@ -5,8 +5,12 @@ import { apiRouter } from '../route/api';
 import cors from "cors";
 
 export const web = express();
+web.use(cors({
+  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  credentials: true,
+}));
+
 web.use(express.json());
-web.use(cors());
 web.use(publicRouter);
 web.use(apiRouter);
 web.use(errorMiddleware);
